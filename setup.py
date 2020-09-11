0e0bd30c9ad30967bcf872c712559e16cbdef3f3
Charles Morton <cgmorton@users.noreply.github.com>
2020-09-11 10:44:48 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     keywords='GDAL OGR',
     classifiers = [
         'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.6'],
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'],
 )
